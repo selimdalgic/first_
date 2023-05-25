ee86c5bba1ea888efd5b79900695c7d9c129d4b3
Selim Dalgic <selimclean@gmail.com>
2023-05-25 18:35:49 +0200

    adding some stuff the second class

--- src/main/java/first_/MySecondClass.java
+++ src/main/java/first_/MySecondClass.java
@@ -3,5 +3,6 @@ package first_;
 public class MySecondClass {
     public static void main(String[] args) {
         System.out.println("My Second Class Bro");
+        System.out.println("Updating some stuff about wedding");
     }
 }
